8e945bb4da32b73a62b8292470a9d7eda3806b63
Huandayne <huan20906@gmail.com>
2026-05-28 21:53:34 +0700

    ham them 1 nut

--- bai tap tuan 12.c
+++ bai tap tuan 12.c
@@ -9,6 +9,15 @@ struct Node {
     struct Node* right;
 };
 
+// Hàm tiện ích để tạo một nút mới
+struct Node* createNode(int value) {
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->namSinh = value;
+    newNode->left = NULL;
+    newNode->right = NULL;
+    return newNode;
+}
+
 int main(){
 
 }
