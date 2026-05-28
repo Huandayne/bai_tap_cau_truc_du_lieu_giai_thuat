62cd747519a4b335083e338d83bfec81aa45a9d0
Huandayne <huan20906@gmail.com>
2026-05-28 13:00:05 +0700

    ham tien ich tao nut moi

--- bai tap tuan 13.c
+++ bai tap tuan 13.c
@@ -1,1 +1,17 @@
-aaa
+#include<stdio.h>
+
+struct Node {
+    int namSinh;
+    struct Node* left;
+    struct Node* right;
+};
+
+// Hàm tiện ích để tạo một nút mới
+struct Node* createNode(int value) {
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->namSinh = value;
+    newNode->left = NULL;
+    newNode->right = NULL;
+    return newNode;
+}
+
